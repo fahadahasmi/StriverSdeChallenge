f869871e8a9952cbcd658d9a28f8b4d212206f5e
Fahad Hasmi <85676923+fahadahasmi@users.noreply.github.com>
2022-06-11 17:19:19 +0530

    Update LongestIncreasingSubSequence.py

--- LongestIncreasingSubSequence.py
+++ LongestIncreasingSubSequence.py
@@ -1,3 +1,27 @@
+#solution one with TC: O(n*n) gives TLE
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        arr = list(set(nums))
+        arr.sort()
+        
+        m = len(nums)
+        n = len(arr)
+        
+        dp = [[0 for x in range(n+1)]for y in range(m+1)]
+        
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                
+                if nums[i-1] == arr[j-1]:
+                    dp[i][j] = 1+ dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+        
+        return dp[-1][-1]
+
+#solution two
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         
