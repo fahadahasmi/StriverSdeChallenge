5193d76b368885ea942e7404892718f82eb49769
Fahad Hasmi <85676923+fahadahasmi@users.noreply.github.com>
2022-06-13 15:43:04 +0530

    Create nMeetingsInOneRoom.py

---
+++ nMeetingsInOneRoom.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+class Solution:
+    
+    #Function to find the maximum number of meetings that can
+    #be performed in a meeting room.
+    def maximumMeetings(self,n,start,end):
+        # code here
+        arr = [i for i in range(n)]
+        
+        arr.sort(key = lambda x: [end[x],start[x],x])
+        
+        maxEnd = end[arr[0]]
+        c = 1
+        
+        for i in range(1,len(arr)):
+            if start[arr[i]] > maxEnd:
+                c+=1 
+                maxEnd = end[arr[i]]
+        
+        return c
