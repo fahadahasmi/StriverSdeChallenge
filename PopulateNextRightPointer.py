9095b6043fadd353d1e5b8403969c2c0f69aea10
Fahad Hasmi <85676923+fahadahasmi@users.noreply.github.com>
2022-06-20 12:42:52 +0530

    Create PopulateNextRightPointer.py

---
+++ PopulateNextRightPointer.py
@@ -0,0 +1,32 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return
+        
+        
+        q = [root]
+        
+        while len(q)>0:
+            node = q.pop(0)
+            
+            if node.left and node.right:
+                node.left.next = node.right
+                if node.next:
+                    node.right.next = node.next.left
+                q.append(node.left)
+                q.append(node.right)
+        return root
+        
+        
+        
+        
